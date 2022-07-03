733bfa4e3d0fa9aa06423fa1f5b6c7636352c32b
gs97ahn <gs97ahn@naver.com>
2022-07-04 03:42:36 +0900

    feat: spalsh screen

---
+++ lib/splash_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import './menu_navigation.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = VideoPlayerController.asset('assets/splash.mp4')
+    ..initialize().then((_) {
+      setState(() {});
+    })
+    ..setVolume(0.0);
+    _playVideo();
+  }
+
+  void _playVideo() async {
+    _controller.play();
+
+    Future.delayed(
+      const Duration(seconds: 3),
+        () {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(
+                  builder: (context) => MenuNavigation()
+          ));
+        }
+    );
+  }
+
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 174, 190, 191),
+      body: Center(
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(
+                _controller
+            )
+        )
+            : Container(),
+      )
+    );
+  }
+
+}
