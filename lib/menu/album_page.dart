ff1f67e433a3c2287d290b687de124b4c88eff06
gs97ahn <gs97ahn@naver.com>
2022-07-08 16:12:43 +0900

    feat: add image slider

--- lib/menu/album_page.dart
+++ lib/menu/album_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class AlbumPage extends StatefulWidget {
   const AlbumPage({Key? key}) : super(key: key);
@@ -8,31 +9,63 @@ class AlbumPage extends StatefulWidget {
 }
 
 class _AlbumPageState extends State<AlbumPage> {
-  final String title = 'Album';
+  final String _title = 'Album';
+  final List<String> _image = [
+    './assets/photo1.jpeg',
+    './assets/photo2.jpeg',
+    './assets/photo3.jpeg',
+    './assets/photo4.png',
+    './assets/photo5.png',
+    './assets/photo6.jpeg',
+    './assets/photo7.jpeg',
+    './assets/photo8.jpeg',
+    './assets/photo9.jpeg',
+    './assets/photo10.jpeg',
+  ];
+
+  List<Widget> _items(List itemList) {
+    return List<Widget>.generate(itemList.length, (int i) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: Image.asset(itemList[i])
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 174, 190, 191),
-      appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 174, 190, 191),
-        title: Text(title,
-          style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 1, 97,112)
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 174, 190, 191),
+          title: Text(_title,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 1, 97, 112)
+            ),
           ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                    padding: const EdgeInsets.only(top: 30, bottom: 30),
+                    child: CarouselSlider(
+                        options: CarouselOptions(
+                            height: 350,
+                            enlargeCenterPage: true,
+                            enableInfiniteScroll: true,
+                            initialPage: 0,
+                            autoPlay: true,
+                            autoPlayInterval: const Duration(seconds: 5),
+                            enlargeStrategy: CenterPageEnlargeStrategy.height),
+                        items: _items(_image)))
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        )
     );
   }
 }
